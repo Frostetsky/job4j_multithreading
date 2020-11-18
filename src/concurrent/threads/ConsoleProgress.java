76093eb9d0814a15d33cd571c120e74c56a326fb
Frostetsky <nesterenko-nikita@bk.ru>
2020-11-19 02:12:35 +0300

    3. Прерывание нити [#412643]

---
+++ src/concurrent/threads/ConsoleProgress.java
@@ -0,0 +1,26 @@
+package concurrent.threads;
+
+public class ConsoleProgress implements Runnable {
+    private final String[] symbols = {".", "..", "..."};
+    private int index = 0;
+
+    @Override
+    public void run() {
+        while (!Thread.currentThread().isInterrupted()) {
+            System.out.print("\rLoading " + symbols[index]);
+            index = (index + 1) % symbols.length;
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(new ConsoleProgress());
+        thread.start();
+        Thread.sleep(5000);
+        thread.interrupt();
+    }
+}
