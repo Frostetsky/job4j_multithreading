8d799822472ceab3e4e0c0d0b0f3e25c1a0baeff
Frostetsky <nesterenko-nikita@bk.ru>
2020-11-19 02:27:09 +0300

    2. Модель памяти Java [#412646]

---
+++ src/concurrent/generalresouces/DCLSingleton.java
@@ -0,0 +1,20 @@
+package concurrent.generalresouces;
+
+public final class DCLSingleton {
+    private static volatile DCLSingleton inst;
+
+    public static DCLSingleton instOf() {
+        if (inst == null) {
+            synchronized (DCLSingleton.class) {
+                if (inst == null) {
+                    inst = new DCLSingleton();
+                }
+            }
+        }
+        return inst;
+    }
+
+
+    private DCLSingleton() {
+    }
+}
